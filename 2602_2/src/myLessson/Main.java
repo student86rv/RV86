3bb4ff53f93297bc0b4275dc89cb611f565eac97
Roman <student.rv86@gmail.com>
2019-02-26 21:15:22 +0200

    javaFX

--- 2602_2/src/myLessson/Main.java
+++ 2602_2/src/myLessson/Main.java
@@ -1,9 +1,14 @@
 package myLessson;
 
 import javafx.application.Application;
+import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -17,8 +22,8 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) {
-        primaryStage.setTitle("Test");
-        Canvas canvas = new Canvas();
+        primaryStage.setTitle("Test"); //заголовок окна
+        Canvas canvas = new Canvas(); //площадка для рисования
         canvas.setWidth(BOARD_WIDTH);
         canvas.setHeight(BOARD_HEIGHT);
         BorderPane group = new BorderPane(canvas);
@@ -27,5 +32,28 @@ public class Main extends Application {
         primaryStage.show();
 
         //canvas.getGraphicsContext2D()
+
+        GraphicsContext gc = canvas.getGraphicsContext2D();
+        gc.setFill(Color.RED);
+        gc.setStroke(Color.BLACK);
+        gc.setLineWidth(2);
+        gc.fillOval(200, 200, 30, 30);
+
+//        gc.setFill(Color.GREEN);
+//        gc.fillRect(250, 250, 30, 30);
+//        gc.strokeOval(100, 100, 30, 30);
+
+        scene.setOnKeyPressed(new EventHandler<KeyEvent>() {
+            @Override
+            public void handle(KeyEvent event) {
+                if (event.getCode() == KeyCode.LEFT) {
+                    System.out.println("left");
+                    //x = x - 5;
+                }
+                if (event.getCode() == KeyCode.RIGHT) {
+                    System.out.println("right");
+                }
+            }
+        });
     }
 }
