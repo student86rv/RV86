adc5a5a8db903bcf32a2a204df861e93d0b6896b
Roman <student.rv86@gmail.com>
2019-02-26 19:54:02 +0200

    thread, synchronized

--- 2602/src/myLessson/Main.java
+++ 2602/src/myLessson/Main.java
@@ -2,20 +2,49 @@ package myLessson;
 
 public class Main {
 
+    public static int count = 0;
+
     public static void main(String[] args) {
 
-        Runnable runnable = new Runnable() {
+//        Runnable runnable = new Runnable() {
+//            @Override
+//            public void run() {
+//                System.out.println("other");
+//
+//            }
+//        };
+//
+//        System.out.println("before");
+//        Thread thread = new Thread(runnable);
+//        thread.start();
+//        System.out.println("after");
+        for (int i = 0; i < 4; i++) {
+            startNewThread();
+
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println(count);
+        }
+    }
+
+    private static void startNewThread() {
+        new Thread(new Runnable() {
             @Override
             public void run() {
-                System.out.println("other");
-                
+                for (int i = 0; i < 1000000; i++) {
+                    //count++;
+                    incCount();
+                }
+
             }
-        };
+        }).start();
+    }
 
-
-        System.out.println("before");
-        Thread thread = new Thread(runnable);
-        thread.start();
-        System.out.println("after");
+    public static synchronized void incCount() {
+        count++;
     }
 }
