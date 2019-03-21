ac62293de8cfa0bbba62704b6807baf208ca34e2
Roman <student.rv86@gmail.com>
2019-03-21 11:06:58 +0200

    add xmlToJson in testMaven2 project

--- 2602_2/src/myLessson/Main.java
+++ 2602_2/src/myLessson/Main.java
@@ -1,4 +1,4 @@
-package myLessson;
+//package myLessson;
 
 import javafx.application.Application;
 import javafx.event.EventHandler;
--- testMaven2/src/main/java/com/company/Main.java
+++ testMaven2/src/main/java/com/company/Main.java
@@ -15,35 +15,47 @@ public class Main {
 
     public static void main(String[] args) {
 
-        User user = new User("Alex", 20);
+        /*User user = new User("Alex", 20);
         User user1 = new User("Vova", 25);
 
         Group group = new Group();
         group.add(user);
-        group.add(user1);
+        group.add(user1);*/
 
-//        System.out.println("test");
-//
 //        User user = new User("Alex", 22);
-//
 //        Gson gson = new GsonBuilder().setPrettyPrinting().create();
 //        String json = gson.toJson(user);
 //        System.out.println(json);
-//
 //        writeTofile(json, "test_file.txt");
-        try {
-            XmlMapper xmlMapper = new XmlMapper();
+        /*String newGroup = readFromFile("group1.xml");
+        System.out.println(newGroup);*/
+		
+		Group group1 = xmlToObject(readFromFile("group1.xml"));
+		writeToFile(objectToJson(group2), group1.json);	
+    }
+	
+	public static Group xmlToObject(String xmlString) {
+		XmlMapper xmlMapper = new XmlMapper();
+		return xmlMapper.readValue(xmlString, Group.class);
+	}
+	
+	public static String objectToJson(Object object) {
+		Gson gson = new GsonBuilder().setPrettyPrinting().create();
+		return gson.toJson(user);
+	}
+
+/*
+	public static void objectToXml(Object object) {
+		try {
+			XmlMapper xmlMapper = new XmlMapper();
             xmlMapper.setVisibility(PropertyAccessor.FIELD, JsonAutoDetect.Visibility.ANY);
             String xml = xmlMapper.writeValueAsString(group);
             writeToFile(xml, "group.xml");
         } catch (JsonProcessingException e) {
             e.printStackTrace();
         }
-
-        String newGroup = readFromFile("group1.xml");
-        System.out.println(newGroup);
-    }
-
+	}*/
+	
     public static void writeToFile(String string, String fileName) {
         try {
             FileUtils.writeStringToFile(
@@ -56,7 +68,6 @@ public class Main {
     }
 
     public static String readFromFile(String fileName) {
-
         String string = "";
         try {
             string = FileUtils.readFileToString(new File(fileName), "UTF-8");
